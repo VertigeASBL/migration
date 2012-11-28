acfb428e6d0c430fba2518a772f983c22d728531
Michel Bystranowski <michel@vertige.org>
2012-11-28 10:11:20 +0100

    commit initial

---
+++ lang/migration_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'migration_titre' => 'API Migration',
+);
+
+?>
---
+++ lang/paquet-migration_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'migration_description' => 'Une boîte à outils pour les migrations des bases de données externes vers SPIP. Contient des exemples de migration de bdd, et de migration des urls.',
+	'migration_nom' => 'API Migration',
+	'migration_slogan' => 'Des outils pour migrer des bases de données tierces vers SPIP.',
+);
+
+?>
